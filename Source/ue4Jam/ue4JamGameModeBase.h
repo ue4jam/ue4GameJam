1195c0b0d0eaef1c1247a4daab332bbe6796d187
MarcusHough <skatedude38@yahoo.com>
2017-11-04 13:03:53 -0500

    New Repository
    Ported Game Over to New Repository

---
+++ Source/ue4Jam/ue4JamGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ue4JamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4JAM_API Aue4JamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
